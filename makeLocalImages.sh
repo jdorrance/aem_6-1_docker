1b774241bf7c0f35890d75c30f539a24ebcb8599
John Dorrance <John Dorrance>
2017-01-13 23:30:45 -0500

    reorg

--- makeLocalImages.sh
+++ makeLocalImages.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-function getinput {
-  while true; do
-	  echo "Put your AEM jar and your license file in ${1}-tar/resources directory."
-      read -p "When ready type y and hit enter or q to quit? " y
-      case $y in
-          [Yy]* ) break;;
-          [Qq]* ) exit 0;;
-          * ) echo "Please answer y when your ready to move foward.";;
-      esac
-  done
-}
+# function getinput {
+#   while true; do
+# 	  echo "Put your AEM jar and your license file in ${1}-tar/resources directory."
+#       read -p "When ready type y and hit enter or q to quit? " y
+#       case $y in
+#           [Yy]* ) break;;
+#           [Qq]* ) exit 0;;
+#           * ) echo "Please answer y when your ready to move foward.";;
+#       esac
+#   done
+# }
 
 function build {
   sh -c 'cd '${1}'/ && exec docker build -t aem_6-1_'$(echo ${1} | sed 's/-.*//')' .'
@@ -29,4 +29,4 @@ build "author"
 build "dispatcher"
 
 sh -c 'cd composedev-tar'
-echo 'Now go into composedev-tar and run this command:./start-containers.sh'
+echo 'Now go into composers and run this command:./start-containers.sh'
